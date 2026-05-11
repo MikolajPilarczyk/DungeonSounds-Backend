9646c591b88678d61a357898dd03bd068c56d678
Forek145 <pilarczyk.miko@gmail.com>
2026-05-11 18:21:38 +0200

    Stworzenie klasy posiadającej pusczenie muzyki za pomocą funkcji do puźniejszczego wykorzystanie tego na stronie

--- src/main/java/com/example/learnlybacked/music/MusicController.java
+++ src/main/java/com/example/learnlybacked/music/MusicController.java
@@ -1,14 +1,29 @@
 package com.example.learnlybacked.music;
 
+import com.example.learnlybacked.playlists.PlaylistController;
+import com.example.learnlybacked.playlists.UserPlaylistsSetTable;
 import dev.arbjerg.lavalink.client.LavalinkClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/api")
 @Service
 public class MusicController
 {
     private final LavalinkClient client;
 
+
+    public static class SongData
+    {
+        public Long id;
+        public boolean playing;
+    }
 
     public MusicController(LavalinkClient client)
     {
@@ -46,6 +61,13 @@ public class MusicController
     public void PlayMusic(long guildId, String trackLink)
     {
         //dodaj dodawanie puszczanie muzyki tutaj
+    }
+
+    @PostMapping("/playsong")
+    public void RecivePlaylistSet(@RequestBody SongData songData)
+    {
+        System.out.println("Wywolano" + songData.id + songData.playing);
+
     }
 
 }
