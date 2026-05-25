db4b22991fa5f839b0866200b40f1b2bd7e703c3
Forek145 <pilarczyk.miko@gmail.com>
2026-05-26 00:55:39 +0200

    fixed bugs related with DiscordAuth implementation

---
+++ src/main/java/com/example/learnlybacked/discordSerwers/DiscordServersDTO.java
@@ -0,0 +1,31 @@
+package com.example.learnlybacked.discordSerwers;
+
+
+import com.example.learnlybacked.user.UserLoginDTO;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "DiscordServers")
+public class DiscordServersDTO {
+    @Id
+    @Column(name = "server_id")
+    private String serverId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private UserLoginDTO user;
+
+    @Column(name = "server_name", nullable = false)
+    private String serverName;
+
+    @Column(name = "server_icon_url")
+    private String serverIconUrl;
+}
